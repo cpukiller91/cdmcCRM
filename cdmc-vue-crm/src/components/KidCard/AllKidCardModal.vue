88dfc829474d88faa4371dba070951a2e7bd4157
Yri Chomich <cpukiller1991@gmail.com>
2021-10-02 22:05:35 +0300

    new form

--- cdmc-vue-crm/src/components/KidCard/AllKidCardModal.vue
+++ cdmc-vue-crm/src/components/KidCard/AllKidCardModal.vue
@@ -145,7 +145,7 @@
                                     :key="i"
                             >
                                 <v-expansion-panel-header>
-                                    {{item.kidf}} {{item.kidi}} {{item.kido}} - количество записей ( {{item.count>1?item.count:"--!!--"}} )
+                                    {{item.kidf}} {{item.kidi}} {{item.kido}} - количество записей ( {{item.count>1?item.count:"!"}} )
 
                                 </v-expansion-panel-header>
                                 <v-expansion-panel-content>
